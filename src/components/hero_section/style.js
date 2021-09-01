9c06aa0d259160e71468ddaf7276ed1cd4a10167
xeonray-origin <amritendu.bhattacharjee@gmail.com>
2021-09-01 22:00:18 +0530

    added hero section and static assets

--- src/components/hero_section/style.js
+++ src/components/hero_section/style.js
@@ -2,73 +2,19 @@ import { createStyles } from '@material-ui/core';
 
 const HeroSectionStyles = (theme) =>
   createStyles({
-    heroTitle: {
-      textAlign: 'right',
-      fontSize: '5rem',
-      [theme.breakpoints.down('sm')]: {
-        fontSize: '2rem',
-        textAlign: 'center',
-        marginTop: theme.spacing(5),
-      },
-      [theme.breakpoints.up('sm')]: {
-        fontSize: '4rem',
-      },
-    },
-    heroSubtitle: {
-      fontSize: '2rem',
-      textAlign: 'right',
-      [theme.breakpoints.down('sm')]: {
-        fontSize: '1.7rem',
-        textAlign: 'center',
-      },
-      [theme.breakpoints.up('sm')]: {
-        fontSize: '2rem',
-      },
-    },
-    titleHolder: {
+    bgVideo: {
+      height: '100%',
       width: '100%',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 0,
-      [theme.breakpoints.down('sm')]: {
-        width: '100%',
-      },
-      // backgroundColor: 'blue',
+      float: 'left',
+      top: 0,
+      padding: 'none',
+      position: 'absolute',
+      objectFit: 'cover',
     },
-    btnContainer: {
-      width: 'fit-content',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 0,
-      [theme.breakpoints.down('sm')]: {
-        width: '12em',
-        display: 'block',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: '8rem',
-      },
-      // backgroundColor: 'red',
-    },
-    actionBtn: {
-      margin: theme.spacing(1),
-      '&:nth-child(2)': {
-        marginRight: 0,
-        [theme.breakpoints.down('sm')]: {
-          display: 'block',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        },
-      },
-    },
-    heroImage: {
-      width: '34.8rem',
-      [theme.breakpoints.down('sm')]: {
-        width: '100%',
-        position: 'absolute',
-        top: 1.7,
-        bottom: 1,
-        zIndex: -1000,
-      },
+    heroRoot: {
+      position: 'relative',
+      height: '80vh',
+      width: '100%',
     },
   });
 
