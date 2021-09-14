9a58343a761c4781ebad92c7bbe8fff1eb1304b9
xeonray-origin <amritendu.bhattacharjee@gmail.com>
2021-09-15 01:33:07 +0530

    fixed routing issue

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { routes } from './routes';
 
 const Layout = () => <>{routes}</>;
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/'>
     <Layout />
   </Router>,
   document.getElementById('root')
